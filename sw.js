9aa255260d546e864f6ab0449c31132c2db75bd6
wangxueqing0427-oss <wangxueqing0427@gmail.com>
2026-09-07 19:53:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 const CACHE='qinghua2027-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./epi.html','./manifest.json','./icon.svg']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
